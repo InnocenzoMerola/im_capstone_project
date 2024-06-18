abe82c049119b86cbbfeac136fd44fb655511a06
Innocenzo Merola <146184569+InnocenzoMerola@users.noreply.github.com>
2024-06-18 19:44:43 +0200

    upload

--- frontend/src/pages/stops/StopDetail.jsx
+++ frontend/src/pages/stops/StopDetail.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import ShowComment from "../../comments/ShowComment";
 import AddComment from "../../comments/AddComment";
+import { Badge } from "react-bootstrap";
 
 const StopDetail = function () {
   const [stopData, setStopData] = useState(null);
@@ -25,15 +26,39 @@ const StopDetail = function () {
     <div>
       {stopData && (
         <>
-          <h1>{stopData.name}</h1>
-          <h2>{stopData.location}</h2>
-          <h3>{stopData.categories[0].name}</h3>
-          <p>{stopData.description_it}</p>
-          <img src={`/storage/${stopData.image}`} alt={stopData.name} />
-          <img src={`/storage/${stopData.image2}`} alt={stopData.name} />
-          <img src={`/storage/${stopData.image3}`} alt={stopData.name} />
-          <img src={`/storage/${stopData.image4}`} alt={stopData.name} />
+          <div className="container stop-detail-cont">
+            <div className="row">
+              <div className="col-6">
+                <div>
+                  <h1>{stopData.name}</h1>
+                </div>
+                <div>
+                  <img src={`/storage/${stopData.image}`} alt={stopData.name} />
+                </div>
+                <div>
+                  <h2>{stopData.location}</h2>
+                </div>
+                <div>
+                  <Badge>{stopData.categories[0].name}</Badge>
+                </div>
+                <div>
+                  <p>{stopData.description_it}</p>
+                </div>
 
+                <div>
+                  <img src={`/storage/${stopData.image2}`} alt={stopData.name} />
+                </div>
+              </div>
+              <div className="col-6">
+                <div>
+                  <img src={`/storage/${stopData.image3}`} alt={stopData.name} />
+                </div>
+                <div>
+                  <img src={`/storage/${stopData.image4}`} alt={stopData.name} />
+                </div>
+              </div>
+            </div>
+          </div>
           <ShowComment stopId={stopData.id} />
           <AddComment stopId={stopData.id} />
         </>
