3c22dd311ef68c149acdb7c52df415a2f1c99e1e
Innocenzo Merola <146184569+InnocenzoMerola@users.noreply.github.com>
2024-06-28 09:54:38 +0200

    update

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -1,9 +1,23 @@
 <?php
 
-use App\Http\Middleware\AdminMiddleware;
+use Dotenv\Dotenv;
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\AdminMiddleware;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
+
+
+    $envFile = '.env';
+
+    if (file_exists(__DIR__ . '/../.env') && getenv('APP_ENV') === 'local') {
+        $envFile = '.env';
+    } elseif (file_exists(__DIR__ . '/../.env.production') && getenv('APP_ENV') === 'production') {
+        $envFile = '.env.production';
+    }
+
+    // Carica le variabili di ambiente dal file .env corretto
+    $dotenv = Dotenv::createImmutable(__DIR__ . '/../', $envFile);
+    $dotenv->load();
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
