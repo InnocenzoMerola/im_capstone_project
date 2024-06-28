0ccb684a52790b90601afb3f80d4dd39cdc58c02
Innocenzo Merola <146184569+InnocenzoMerola@users.noreply.github.com>
2024-06-28 09:57:51 +0200

    up

--- backend/bootstrap/app.php
+++ backend/bootstrap/app.php
@@ -7,17 +7,17 @@ use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
 
-    $envFile = '.env';
+    // $envFile = '.env';
 
-    if (file_exists(__DIR__ . '/../.env') && getenv('APP_ENV') === 'local') {
-        $envFile = '.env';
-    } elseif (file_exists(__DIR__ . '/../.env.production') && getenv('APP_ENV') === 'production') {
-        $envFile = '.env.production';
-    }
+    // if (file_exists(__DIR__ . '/../.env') && getenv('APP_ENV') === 'local') {
+    //     $envFile = '.env';
+    // } elseif (file_exists(__DIR__ . '/../.env.production') && getenv('APP_ENV') === 'production') {
+    //     $envFile = '.env.production';
+    // }
 
-    // Carica le variabili di ambiente dal file .env corretto
-    $dotenv = Dotenv::createImmutable(__DIR__ . '/../', $envFile);
-    $dotenv->load();
+    // // Carica le variabili di ambiente dal file .env corretto
+    // $dotenv = Dotenv::createImmutable(__DIR__ . '/../', $envFile);
+    // $dotenv->load();
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
